28633c37738b77898479ce6172bfb6d66b807652
yeilop44 <pc_ylopez@hotmail.com>
2019-04-17 16:51:42 -0500

    make it better

--- api/database/database.js
+++ api/database/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose'); 
 
-//const URI = 'mongodb://yeilop44:Fragante44@ds245772.mlab.com:45772/meansearchflights';
-//const URI = 'mongodb://yeilop44:Fragante44@ds143326.mlab.com:43326/politicalmap';
-const URI = 'mongodb://localhost:27017/products-shop';
+
+const URI = 'mongodb://yeilop44:Fragante44@ds143326.mlab.com:43326/politicalmap';
+//const URI = 'mongodb://localhost:27017/products-shop';
  
 mongoose.connect(URI)
 	.then(db => console.log('db is connected now'))
--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 //app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json());
 app.use(cors());
-app.use(cors({origin: 'https://back-mpolitical.herokuapp.com/'}));
+app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
 app.use('/affiliates', affiliateRoutes);
