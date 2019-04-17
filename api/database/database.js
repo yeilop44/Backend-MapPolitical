acafc44ceb1ebf8366536dea8eb14f993bae885d
yeilop44 <pc_ylopez@hotmail.com>
2019-04-17 12:54:04 -0500

    deploy server

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-const URI = 'mongodb://yeilop44:Fragante44@ds245772.mlab.com:45772/meansearchflights';
+//const URI = 'mongodb://yeilop44:Fragante44@ds245772.mlab.com:45772/meansearchflights';
 const URI = 'mongodb://yeilop44:Fragante44@ds143326.mlab.com:43326/politicalmap';
 //const URI = 'mongodb://localhost:27017/products-shop';
  
