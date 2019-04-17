5fa4e4e9b670326f454e9599e3f8de7105a76b65
yeilop44 <pc_ylopez@hotmail.com>
2019-04-17 12:52:05 -0500

    deploy server

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose'); 
 
 const URI = 'mongodb://yeilop44:Fragante44@ds245772.mlab.com:45772/meansearchflights';
+const URI = 'mongodb://yeilop44:Fragante44@ds143326.mlab.com:43326/politicalmap';
 //const URI = 'mongodb://localhost:27017/products-shop';
  
 mongoose.connect(URI)
