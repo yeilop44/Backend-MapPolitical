25380f0d2298abbf78fae3e77fa7b8c7af368414
yeilop44 <pc_ylopez@hotmail.com>
2019-04-23 22:45:34 -0500

    make it better

--- api/routes/user.js
+++ api/routes/user.js
@@ -77,6 +77,14 @@ router.get('/', ensureToken, async (req, res) => {
 function ensureToken(req, res, next){
     const bearerHeader = req.headers['authorization'];
     console.log(bearerHeader);
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else{
+        res.endStatus(403);
+    }
 }
 
 
