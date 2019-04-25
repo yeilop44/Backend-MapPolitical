dfb3b3ba7a93f6566755b652a582769d3530be78
yeilop44 <pc_ylopez@hotmail.com>
2019-04-25 14:36:12 -0500

    make it better

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 //app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
