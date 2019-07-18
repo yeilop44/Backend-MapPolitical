2bcca975b9c5bd10297e0b8422eac71b9e5feeb9
Jonathan Velez <mrvelez13@gmail.com>
2019-07-17 23:05:25 -0500

    agrego métodos http que faltaban

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/uploadApi', uploadDos);
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
