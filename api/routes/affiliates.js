90684778fd1b9d42202d77f86f988da87b1659a1
ylopez <yalopez@unac.edu.co>
2019-08-09 14:20:30 -0500

    ok token

--- api/routes/affiliates.js
+++ api/routes/affiliates.js
@@ -98,7 +98,7 @@ router.get('/:userName/:page',async (req, res) => {
     res.status(200).json({
          message: 'Found Affiliates',
          Count: count,
-         Affiliates: affiliate,
+         affiliates: affiliate,
          pager,
          pageOfItems
     });
