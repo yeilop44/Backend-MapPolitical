be4794626c683c3149cb6f760696d3573de9ce1f
ylopez <yalopez@unac.edu.co>
2019-08-26 20:00:28 -0500

    cors aws 2

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(session({
 //app.use(cors({origin: 'http://localhost:4200'}));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://mappoliticalfrontend-env.semmrdp3zc.us-east-2.elasticbeanstalk.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
