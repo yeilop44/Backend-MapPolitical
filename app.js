f98544ff28a479d916ebc4667bef6666d7e5240d
ylopez <yalopez@unac.edu.co>
2019-08-31 11:05:51 -0500

    cristobal

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //app.use(cors());
-app.use(cors({origin: 'http://mappoliticalfrontend-env.semmrdp3zc.us-east-2.elasticbeanstalk.com', credentials: true}));
+app.use(cors({origin: 'http://localhost:4200', credentials: true}));
 app.use(session({ 
     secret: 'keyboard cat', 
     resave: true, 
